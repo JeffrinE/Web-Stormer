23fe9c7988da022ef09b66669a38769008480191
Jeffrin E <jeffrinedmond@gmail.com>
2025-02-26 15:09:08 +0530

    modified schema

--- src/dbcreator.py
+++ src/dbcreator.py
@@ -1,35 +1,42 @@
 import sqlite3
 
-conn = sqlite3.connect(r"..\database\urls.db")
+conn = sqlite3.connect("..\\database\\urls.db")
 
 cur = conn.cursor()
 
 cur.execute("""
             CREATE TABLE IF NOT EXISTS urls(
-            id INTEGER PRIMARY KEY,
+            id varchar(255) PRIMARY KEY,
             url varchar(255) NOT NULL,
-            date text
+            date varchar(255)
             ) 
 
 """)
-# cur.execute("INSERT INTO urls (url, date) VALues (?, ?)", ("www.example.com", "1234.32.32"))
 
-def add_to_db(url: str, date: str) -> None:
-    with sqlite3.connect(r"..\database\urls.db") as conn:
+def add_to_db(row_id: str, url: str, date: str) -> None:
+    with sqlite3.connect("..\\database\\urls.db") as conn:
         cur = conn.cursor()
-        cur.execute("INSERT INTO urls (url, date) VALUES ( ? , ?)", (url, date))
+        cur.execute("INSERT INTO urls (id, url, date) VALUES (?, ? ,?)", (row_id ,url, date))
         conn.commit()
 
-def remove_from_db(url: str) -> None:
-    with sqlite3.connect(r"..\database\urls.db") as conn:
+def remove_from_db(row_id: str) -> None:
+    with sqlite3.connect("..\\database\\urls.db") as conn:
         cur = conn.cursor()
-        cur.execute(f"DELETE FROM urls WHERE url = ? ;", (url, ))
+        cur.execute(f"DELETE FROM urls WHERE id = ? ;", (row_id, ))
         conn.commit()
 
 def show_blocked() -> list:
-    with sqlite3.connect(r"..\database\urls.db") as conn:
+    with sqlite3.connect("..\\database\\urls.db") as conn:
         cur = conn.cursor()
         cur.execute(f"SELECT * FROM urls;")
-        conn.commit()
         urls = cur.fetchall()
+        conn.commit()
+    return urls
+
+def show_from_id(row_id):
+    with sqlite3.connect("..\\database\\urls.db") as conn:
+        cur = conn.cursor()
+        cur.execute(f"SELECT url FROM urls where id = ? ;", (row_id, ))
+        urls = cur.fetchone()
+        conn.commit()
     return urls
